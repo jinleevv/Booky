03c252b27aa16f6f312501e16d5e327dccc08113
Jinwon Lee <jinwon.lee@mail.mcgill.ca>
2024-11-16 23:55:01 -0500

    update login page

---
+++ src/features/Login/LoginForm.tsx
@@ -0,0 +1,142 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { auth } from "../../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+interface PasswordRequirement {
+  match: boolean;
+  text: string;
+}
+
+interface PasswordRequirements {
+  length: PasswordRequirement;
+  number: PasswordRequirement;
+  special: PasswordRequirement;
+}
+
+const formSchema = z.object({
+  email: z.string().min(1),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .regex(/[0-9]/, "Password must contain at least 1 number")
+    .regex(
+      /[^a-zA-Z0-9]/,
+      "Password must contain at least 1 special character"
+    ),
+});
+
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const [password, setPassword] = useState<string>("");
+  const [requirements, setRequirements] = useState<PasswordRequirements>({
+    length: { match: false, text: "At least 8 characters" },
+    number: { match: false, text: "Contains at least 1 number" },
+    special: { match: false, text: "Contains at least 1 special character" },
+  });
+
+  useEffect(() => {
+    setRequirements({
+      length: {
+        match: password.length >= 8,
+        text: "At least 8 characters",
+      },
+      number: {
+        match: /[0-9]/.test(password),
+        text: "Contains at least 1 number",
+      },
+      special: {
+        match: /[^a-zA-Z0-9]/.test(password),
+        text: "Contains at least 1 special character",
+      },
+    });
+  }, [password]);
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      values.email,
+      values.password
+    );
+    console.log(response);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl className="w-3/4">
+                <Input placeholder="booky@mail.mcgill.ca" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl className="w-3/4">
+                <Input
+                  type="password"
+                  placeholder="••••••••"
+                  {...field}
+                  onChange={(e) => {
+                    field.onChange(e);
+                    setPassword(e.target.value);
+                  }}
+                />
+              </FormControl>
+              <div className="text-sm mt-2 space-y-1">
+                {Object.entries(requirements).map(([key, requirement]) => (
+                  <div key={key} className="flex items-center gap-2">
+                    <span
+                      className={
+                        requirement.match ? "text-green-500" : "text-gray-400"
+                      }
+                    >
+                      {requirement.match ? "✓" : "○"}
+                    </span>
+                    <span
+                      className={
+                        requirement.match ? "text-green-500" : "text-gray-400"
+                      }
+                    >
+                      {requirement.text}
+                    </span>
+                  </div>
+                ))}
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-end w-3/4">
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,8 +4,12 @@ import NavigationBar from "../features/NavigationBar";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import LoginForm from "@/features/Login/LoginForm";
 
 export default function Login() {
+  const navigate = useNavigate();
+
   return (
     <section className="h-screen w-screen bg-white px-8 py-4">
       <div className="flex items-center gap-2">
@@ -15,15 +19,26 @@ export default function Login() {
       <div className="flex flex-col-2 items-center justify-center h-5/6 w-full mt-12">
         <div className="flex flex-col h-full w-1/2 py-12">
           <div className="flex w-full">
-            <Button variant="ghost" className="gap-1.5 w-20 rounded-xl">
+            <Button
+              variant="ghost"
+              className="gap-1.5 w-20 rounded-xl"
+              onClick={() => navigate("/")}
+            >
               <HiOutlineArrowNarrowLeft size={25} />
               Back
             </Button>
           </div>
-          <div>
-            <Label className="text-3xl font-bold">
-              Create your Booky account
-            </Label>
+          <div className="space-y-4">
+            <div>
+              <Label className="text-3xl font-bold">
+                Create your Booky account
+              </Label>{" "}
+              <br />
+              <Label className="text-sm text-gray-500">
+                Free for individuals. Team plans for collaborative features.
+              </Label>
+            </div>
+            <LoginForm />
           </div>
         </div>
         <div className="flex flex-col items-center justify-center h-full w-1/2">
