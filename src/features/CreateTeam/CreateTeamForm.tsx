362e105704707d10efbfa399bd0471014c5cc78a
Jinwon Lee <jinwon.lee@mail.mcgill.ca>
2024-12-10 11:55:47 -0500

    update faster form

--- src/features/CreateTeam/CreateTeamForm.tsx
+++ src/features/CreateTeam/CreateTeamForm.tsx
@@ -1,11 +1,10 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -66,25 +65,30 @@ export default function CreateTeamForm() {
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   }
   return (
     <section className="grid mt-10 bg-white">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
           <FormField
             control={form.control}
             name="teamName"
             render={({ field }) => (
-              <FormItem className="flex w-full">
-                <FormLabel className="w-24 mt-auto mb-auto">
-                  Team Name
-                </FormLabel>
-                <FormControl>
-                  <Input className="w-1/3" placeholder="Team Name" {...field} />
-                </FormControl>
+              <FormItem>
+                <div className="flex w-full">
+                  <FormLabel className="w-24 mt-auto mb-auto">
+                    Team Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="w-1/3"
+                      placeholder="Team Name"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
                 <FormMessage />
               </FormItem>
             )}
@@ -97,20 +101,24 @@ export default function CreateTeamForm() {
                 <div className="space-y-4">
                   {form.watch("schedule").map((day, dayIndex) => (
                     <div key={day.day} className="flex items-center space-x-4">
-                      {/* Switch and Day Label */}
-                      <div className="flex w-32 items-center space-x-2">
-                        <Controller
-                          control={form.control}
-                          name={`schedule.${dayIndex}.enabled`}
-                          render={({ field }) => (
-                            <Switch
-                              checked={field.value}
-                              onCheckedChange={field.onChange}
-                            />
-                          )}
-                        />
-                        <FormLabel className="font-medium">{day.day}</FormLabel>
-                      </div>
+                      <FormField
+                        control={form.control}
+                        name={`schedule.${dayIndex}.enabled`} // Explicitly map 'enabled'
+                        render={({ field }) => (
+                          <FormItem className="flex w-32 items-center space-x-2">
+                            <FormControl>
+                              <Switch
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                                className="mt-2"
+                              />
+                            </FormControl>
+                            <FormLabel className="font-medium">
+                              {day.day}
+                            </FormLabel>
+                          </FormItem>
+                        )}
+                      />
 
                       {/* Time Selects */}
                       {day.enabled && (
@@ -121,56 +129,67 @@ export default function CreateTeamForm() {
                               className="flex items-center space-x-2"
                             >
                               {/* Start Time */}
-                              <Controller
+                              <FormField
                                 control={form.control}
-                                name={`schedule.${dayIndex}.times.${timeIndex}.start`}
+                                name={`schedule.${dayIndex}.times.${timeIndex}.start`} // Target nested start field
                                 render={({ field }) => (
-                                  <Select
-                                    value={field.value}
-                                    onValueChange={field.onChange}
-                                  >
-                                    <SelectTrigger className="w-28">
-                                      <SelectValue placeholder="Start Time" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                      {availableTime.map((timeOption) => (
-                                        <SelectItem
-                                          key={timeOption}
-                                          value={timeOption}
-                                        >
-                                          {timeOption}
-                                        </SelectItem>
-                                      ))}
-                                    </SelectContent>
-                                  </Select>
+                                  <FormItem>
+                                    <FormControl>
+                                      <Select
+                                        value={field.value}
+                                        onValueChange={field.onChange}
+                                      >
+                                        <SelectTrigger className="w-28">
+                                          <SelectValue placeholder="Start Time" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                          {availableTime.map((timeOption) => (
+                                            <SelectItem
+                                              key={timeOption}
+                                              value={timeOption}
+                                            >
+                                              {timeOption}
+                                            </SelectItem>
+                                          ))}
+                                        </SelectContent>
+                                      </Select>
+                                    </FormControl>
+                                    <FormMessage />{" "}
+                                    {/* Display any validation error */}
+                                  </FormItem>
                                 )}
                               />
 
                               <span className="text-gray-500">-</span>
 
                               {/* End Time */}
-                              <Controller
+                              <FormField
                                 control={form.control}
-                                name={`schedule.${dayIndex}.times.${timeIndex}.end`}
+                                name={`schedule.${dayIndex}.times.${timeIndex}.end`} // Target nested start field
                                 render={({ field }) => (
-                                  <Select
-                                    value={field.value}
-                                    onValueChange={field.onChange}
-                                  >
-                                    <SelectTrigger className="w-28">
-                                      <SelectValue placeholder="End Time" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                      {availableTime.map((timeOption) => (
-                                        <SelectItem
-                                          key={timeOption}
-                                          value={timeOption}
-                                        >
-                                          {timeOption}
-                                        </SelectItem>
-                                      ))}
-                                    </SelectContent>
-                                  </Select>
+                                  <FormItem>
+                                    <FormControl>
+                                      <Select
+                                        value={field.value}
+                                        onValueChange={field.onChange}
+                                      >
+                                        <SelectTrigger className="w-28">
+                                          <SelectValue placeholder="End Time" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                          {availableTime.map((timeOption) => (
+                                            <SelectItem
+                                              key={timeOption}
+                                              value={timeOption}
+                                            >
+                                              {timeOption}
+                                            </SelectItem>
+                                          ))}
+                                        </SelectContent>
+                                      </Select>
+                                    </FormControl>
+                                    <FormMessage />
+                                  </FormItem>
                                 )}
                               />
 
@@ -216,7 +235,48 @@ export default function CreateTeamForm() {
               </div>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <FormField
+            control={form.control}
+            name="durations"
+            render={({ field }) => (
+              <FormItem className="flex">
+                <div className="flex w-20 space-x-2 mt-auto mb-auto">
+                  <FormLabel>Duration:</FormLabel>
+                </div>
+                <div className="flex border w-fit rounded-md py-1 px-0.5 gap-1">
+                  {["5m", "15m", "30m", "45m", "1h"].map((time) => {
+                    // Check if the time is already in the array
+                    const isSelected = field.value.includes(time);
+
+                    return (
+                      <Button
+                        key={time}
+                        type="button"
+                        variant="ghost"
+                        onClick={() => {
+                          const updatedDurations = isSelected
+                            ? field.value.filter((val: string) => val !== time)
+                            : [...field.value, time];
+                          field.onChange(updatedDurations);
+                        }}
+                        className={`${
+                          isSelected
+                            ? "text-red-700 border border-red-700"
+                            : "text-gray-700 hover:text-gray-900"
+                        }`}
+                      >
+                        {time}
+                      </Button>
+                    );
+                  })}
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex w-full justify-end">
+            <Button type="submit">Submit</Button>
+          </div>
         </form>
       </Form>
     </section>
