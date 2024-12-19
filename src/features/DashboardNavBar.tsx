303ca850b619a30b61a3545dcc2430e23843dca2
Junsoo Kim <junsookim047@gmail.com>
2024-12-19 11:53:13 -0500

    Make meeting dashboard responsive

--- src/features/DashboardNavBar.tsx
+++ src/features/DashboardNavBar.tsx
@@ -6,7 +6,7 @@ export default function NavigationBar() {
   const location = useLocation();
 
   return (
-    <nav className="flex flex-col h-screen w-1/6 py-4 bg-zinc-50 border-r border-gray-200">
+    <nav className="flex flex-col h-screen w-1/6 min-w-[147px] py-4 bg-zinc-50 border-r border-gray-200">
       <div className="flex items-center gap-2 px-8">
         <img src="/mcgill.png" alt="McGill Logo" className="w-10 h-10" />
         <div className="text-sm font-bold text-black -ml-4 -mt-1.5">Booky</div>
