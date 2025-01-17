bcb9cd7b84ee64877a5c9dd9e5143183fa7820ae
Jin Won Lee <jinwonlee0916@gmail.com>
2025-01-17 00:15:37 -0500

    done meeting minute feature. need to connect with the platform

---
+++ server/src/models/meetingMinute.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+interface IMeetingMinute extends Document {
+  _id: string;
+  data: Object;
+  createdAt: Date;
+}
+
+const MeetingMinuteSchema: Schema = new Schema<IMeetingMinute>({
+  _id: { type: String, required: true },
+  data: { type: Object, required: true },
+  createdAt: { type: Date },
+});
+
+// Create a model instance, to be used to interact with MongoDB users collection.
+const MeetingMinute: Model<IMeetingMinute> = mongoose.model<IMeetingMinute>(
+  "MeetingMinute",
+  MeetingMinuteSchema
+);
+
+export default MeetingMinute;
