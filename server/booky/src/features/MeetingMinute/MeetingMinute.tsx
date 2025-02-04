0eea868cdfe015f04dd4a7f5060cb58287bf9894
Jin Won Lee <jinwonlee0916@gmail.com>
2025-02-03 22:06:24 -0500

    Try this

--- server/booky/src/features/MeetingMinute/MeetingMinute.tsx
+++ server/booky/src/features/MeetingMinute/MeetingMinute.tsx
@@ -107,7 +107,7 @@ export default function MeetingMinute() {
   useEffect(() => {
     // const s = io("http://localhost:4001");
     // const s = io("http://localhost:4001");
-    const s = io("https://booky.im:4001", {
+    const s = io("https://booky.im", {
       path: "/socket.io",
       secure: true,
       reconnection: true,
